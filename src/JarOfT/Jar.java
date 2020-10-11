677cfbccf937652263c37f4947b29f064518f2e2
mis6us <mis6us@icloud.com>
2020-10-11 23:32:30 +0300

    Initial commit

---
+++ src/JarOfT/Jar.java
@@ -0,0 +1,23 @@
+package JarOfT;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Jar<T> {
+
+    private Deque<T> elements;
+
+    public Jar() {
+        this.elements = new ArrayDeque<>();
+    }
+
+    public void add(T element) {
+        this.elements.push(element);
+    }
+
+    public T remove() {
+        T element = this.elements.pop();
+
+        return element;
+    }
+}
---
+++ src/JarOfT/Main.java
@@ -0,0 +1,12 @@
+package JarOfT;
+
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
